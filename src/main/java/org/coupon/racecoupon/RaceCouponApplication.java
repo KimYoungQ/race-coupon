31fa799c845ef511bf25769f0007d6ae47731855
KimYoungQ <dudeb0206@daum.net>
2026-07-04 15:28:54 +0900

    git init

---
+++ src/main/java/org/coupon/racecoupon/RaceCouponApplication.java
@@ -0,0 +1,13 @@
+package org.coupon.racecoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/coupon/racecoupon/RaceCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coupon.racecoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
