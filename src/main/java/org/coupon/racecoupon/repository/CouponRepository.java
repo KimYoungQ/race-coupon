4b3f2ba5f3b7f566fc20eafe0c34ad840a3ae8c0
KimYoungQ <dudeb0206@daum.net>
2026-07-05 14:16:02 +0900

    refactor: 비관적 락 조회를 QueryDSL로 변경

--- src/main/java/org/coupon/racecoupon/repository/CouponRepository.java
+++ src/main/java/org/coupon/racecoupon/repository/CouponRepository.java
@@ -1,17 +1,7 @@
 package org.coupon.racecoupon.repository;
 
-import jakarta.persistence.LockModeType;
 import org.coupon.racecoupon.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select c from Coupon c where c.id = :id")
-    Optional<Coupon> findWithPessimisticLockById(@Param("id") Long id);
+public interface CouponRepository extends JpaRepository<Coupon, Long>, CouponRepositoryCustom {
 }
