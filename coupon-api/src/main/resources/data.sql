ae20e06cd2237e9f7966f4e77ce73351fd4a80c4
KimYoungQ <dudeb0206@daum.net>
2026-07-18 14:14:47 +0900

    fix: 시드 데이터에 할인 컬럼 추가

--- coupon-api/src/main/resources/data.sql
+++ coupon-api/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO coupon (title, total_quantity, issued_quantity)
-VALUES ('선착순 쿠폰', 100, 0);
+INSERT INTO coupon (title, total_quantity, issued_quantity, discount_type, discount_value)
+VALUES ('선착순 쿠폰', 100, 0, 'PERCENT', 10);
